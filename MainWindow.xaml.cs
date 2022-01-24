5d297346d8cf7a47f152a4cee8cd65bd2d8ff8cc
Anatoly Sadowsky <sadowsky83@gmail.com>
2022-01-24 22:04:57 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_task14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProdName = "Молоко",
+                ProdPrice = 82,
+                ProdImage = "Data/Milk.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Колбаса",
+                ProdPrice = 320,
+                ProdImage = "Data/kolbasa.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Яйца",
+                ProdPrice = 186,
+                ProdImage = "Data/eggs.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Плита",
+                ProdPrice = 37000,
+                ProdImage = "Data/plita.png",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Стиральная машина",
+                ProdPrice = 27000,
+                ProdImage = "Data/stir.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Телевизор",
+                ProdPrice = 85000,
+                ProdImage = "Data/tv.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_task14
+{
+    public enum ProductCategorys
+    {
+        Food,
+        Appliances
+    }
+
+    public class Product
+    {
+        public string ProdName { get; set; }
+        public decimal ProdPrice { get; set; }
+        public string ProdImage { get; set; }
+        public ProductCategorys ProductCategory { get; set; }
+
+        public override string ToString()
+        {
+            return ProdName;
+        }
+    }
+}
